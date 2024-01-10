7b7652ad76865a9b6da28d9294b72fcf746cb37b
Matthew Krichmar <matthewkrichmar@matthews-air.myfiosgateway.com>
2024-01-10 18:53:25 -0500

    created an object

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const userInputString = prompt(
     "Please enter your desired frozen yogurt flavors separated by commas.",
+    'vanilla,coffee,tart'
   );
+
+  const froyoOrder = {
+    vanilla: 0,
+    coffee: 0,
+    tart: 0,
+  }
   
-  // Split the string of numbers into an array of strings.
-  const stringArray = userInputString.split(",");
+  const orderArray = userInputString.split(",");
   
-  // Convert the array of strings into an array of flavors.
   const flavors = [];
-  for (let i = 0; i < stringArray.length; i++) {
-    const str = stringArray[i];
+  for (let i = 0; i < orderArray.length; i++) {
+    const str = orderArray[i];
     const flavor = str;
     flavors.push(flavor);
   }
 
-  console.table(flavors);
-  
+
+
+
